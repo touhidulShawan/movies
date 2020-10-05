ed5e86eecb0f23fd628469be60275800e0da31b8
Touhidul Shawan <touhidulshawan@gmail.com>
2020-10-05 20:51:29 +0600

    Updates

---
+++ precache-manifest.5da9abd5ab3c79b542d39126a2720e8c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e03defa356f195cc752b4fe1fdee5ac",
+    "url": "/movies/index.html"
+  },
+  {
+    "revision": "87129fa9e08eed98765e",
+    "url": "/movies/static/css/2.fdfe9697.chunk.css"
+  },
+  {
+    "revision": "87129fa9e08eed98765e",
+    "url": "/movies/static/js/2.fc284833.chunk.js"
+  },
+  {
+    "revision": "9c2b079c7c507ea3f72ef59109b49d3d",
+    "url": "/movies/static/js/2.fc284833.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "022e9cb3975f3b34388a",
+    "url": "/movies/static/js/main.f1e9b359.chunk.js"
+  },
+  {
+    "revision": "04d8541a5eac01d4096a",
+    "url": "/movies/static/js/runtime-main.e256fac8.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/movies/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/movies/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/movies/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/movies/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/movies/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
